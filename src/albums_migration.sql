c0d2806f567d5133dde381534960e840a10c435e
Matthew Wiram <matthew.wiram551@gmail.com>
2022-12-02 18:03:27 -0500

    done

---
+++ src/albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+    DROP IF EXISTS albums;
+
+    CREATE TABLE IF NOT EXISTS albums (
+        id INT not null,
+        artist VARCHAR(30),
+        name VARCHAR(30),
+        release_date int not null,
+        sales FLOAT not null,
+        genre VARCHAR(30),
+        PRIMARY KEY (id)
+
+    )
